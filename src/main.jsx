ecbbf03ed9f4669ffd56f1c0b211c08a9b9d6ad3
Oseungli <oseungli100@gmail.com>
2026-07-28 16:19:57 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // 사용 중인 CSS 파일명으로 변경 (없으면 삭제)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
